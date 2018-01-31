678c6ffbccbcc9768f1614a3b43b60626c4a968b
gyver <gggvgyver@gmail.com>
2018-01-31 22:10:49 +0900

    집에서 연습 java fx MVC 연습 180131

--- src/Button_MVC/Main.java
+++ src/Button_MVC/Main.java
@@ -1,16 +1,20 @@
 package Button_MVC;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("Main.fxml"))));
+        stage.setTitle("버튼을 누르세요 MVC 테스트 180131 내방에서");
+        stage.show();
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-
+    public static void main(String[] args) {
+        launch(args);
     }
 }
--- src/Button_MVC/MainController.java
+++ src/Button_MVC/MainController.java
@@ -1,4 +1,14 @@
 package Button_MVC;
 
+import javafx.event.ActionEvent;
+
+import java.util.Random;
+
 public class MainController {
+
+    public void generateRandom(ActionEvent event) {
+        Random rand = new Random();
+        int myrand = rand.nextInt(50) + 1;
+        System.out.println(Integer.toString(myrand));
+    }
 }
