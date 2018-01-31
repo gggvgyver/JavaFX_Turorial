3ca1575ca3aa7e25170037a36041f54d16d379f8
gyver <gggvgyver@gmail.com>
2018-01-31 22:31:29 +0900

    집에서 연습 java fx MVC 연습 180131

--- src/Button_MVC/Main.java
+++ src/Button_MVC/Main.java
@@ -2,16 +2,24 @@ package Button_MVC;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("Main.fxml"))));
-        stage.setTitle("버튼을 누르세요 MVC 테스트 180131 내방에서");
-        stage.show();
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+            Scene scene = new Scene(root, 600, 400);
+            scene.getStylesheets().add(getClass().getResource("Main.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("내방에서 자바FX MVC 연습 180131");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
