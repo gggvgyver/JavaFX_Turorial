aef14dddd8fccca45233b3e905f6b0645734a99d
gyver <gggvgyver@gmail.com>
2018-02-01 16:29:51 +0900

    도서관에서 180201

--- src/BlackJack/Main.java
+++ src/BlackJack/Main.java
@@ -9,14 +9,14 @@ public class Main {
     private static String name;//name of the user
 
     public static void main(String[] args) {
-        System.out.println("Hi! What is your name?");
+        System.out.println("이름을 입력해 주세요!");
         Scanner scan = new Scanner(System.in);
         name = scan.nextLine();
-        System.out.println("Hello, " + name + ", lets play some BlackJack!");
-        System.out.println("How much cash do you want to start with?");
+        System.out.println("안녕, " + name + ", 블랙잭 하자!");
+        System.out.println("돈얼마 가지고 있어?");
         Scanner money = new Scanner(System.in);
         cash = money.nextInt();
-        System.out.println("You start with cash: " + cash);
+        System.out.println("게임시작 금액: " + cash);
         while (cash > 0) {
             Deck deck = new Deck();//initialize deck, dealer, hands, and set the bet.
             deck.shuffle();
@@ -25,57 +25,57 @@ public class Main {
             List<Card> hand = new ArrayList<>();
             hand.add(deck.drawCard());
             hand.add(deck.drawCard());
-            System.out.println("How much would you like to bet?");
+            System.out.println("배팅 얼마 걸거니?");
             bet = Bet(cash);
             System.out.println("Cash:" + (cash - bet));
-            System.out.println("Money on the table:" + bet);
-            System.out.println("Here is your hand: ");
+            System.out.println("테이블에 놓인 배팅금액:" + bet);
+            System.out.println("너의 패: ");
             System.out.println(hand);
             int handvalue = calcHandValue(hand);
-            System.out.println("The dealer is showing: ");
+            System.out.println("딜러 오픈: ");
             dealer.showFirstCard();
             if (hasBlackJack(handvalue) && dealer.hasBlackJack())//check if both the user and dealer have blackjack.
             {
                 Push();
             } else if (hasBlackJack(handvalue))//check if the user has blackjack.
             {
-                System.out.println("You have BlackJack!");
+                System.out.println("축하해 블랙잭이야!");
                 System.out.println("You win 2x your money back!");
                 cash = cash + bet;
                 Win();
             } else if (dealer.hasBlackJack())//check if the dealer has blackjack.
             {
-                System.out.println("Here is the dealer's hand:");
+                System.out.println("딜러 패:");
                 dealer.showHand();
                 Lose();
             } else {
                 if (2 * bet < cash)//check if the user can double down.
                 {
-                    System.out.println("Would you like to double down?");//allows the user to double down.
+                    System.out.println("더블다운 할거니?");//allows the user to double down.
                     Scanner doubledown = new Scanner(System.in);
                     String doubled = doubledown.nextLine();
                     while (!isyesorno(doubled)) {
-                        System.out.println("Please enter yes or no.");
+                        System.out.println("yes 혹은 no 을 입력해도!");
                         doubled = doubledown.nextLine();
                     }
                     if (doubled.equals("yes")) {
-                        System.out.println("You have opted to double down!");
+                        System.out.println("더블다운배팅중!");
                         bet = 2 * bet;
                         System.out.println("Cash:" + (cash - bet));
-                        System.out.println("Money on the table:" + bet);
+                        System.out.println("테이블에 놓인돈:" + bet);
                     }
                 }
-                System.out.println("Would you like to hit or stand?");//ask if the user will hit or stand
+                System.out.println("hit 혹은 stand 결정해도!");//ask if the user will hit or stand
                 Scanner hitorstand = new Scanner(System.in);
                 String hitter = hitorstand.nextLine();
                 while (!isHitorStand(hitter)) {
-                    System.out.println("Please enter 'hit' or 'stand'.");
+                    System.out.println("hit 혹은 stand 를 입력해!");
                     hitter = hitorstand.nextLine();
                 }
                 while (hitter.equals("hit"))//hits the user as many times as he or she pleases.
                 {
                     Hit(deck, hand);
-                    System.out.println("Your hand is now:");
+                    System.out.println("현재 너의 패는:");
                     System.out.println(hand);
                     handvalue = calcHandValue(hand);
                     if (checkBust(handvalue))//checks if the user busted
@@ -88,14 +88,14 @@ public class Main {
                         fivecardtrick();
                         break;
                     }
-                    System.out.println("Would you like to hit or stand?");
+                    System.out.println("hit 할래? stand 할래? hit 혹은 stand 입력해라!!!");
                     hitter = hitorstand.nextLine();
                 }
                 if (hitter.equals("stand"))//lets the user stand.
                 {
                     int dealerhand = dealer.takeTurn(deck);//takes the turn for the dealer.
                     System.out.println("");
-                    System.out.println("Here is the dealer's hand:");
+                    System.out.println("현재 딜러의 패:");
                     dealer.showHand();
                     if (dealerhand > 21)//if the dealer busted, user wins.
                     {
@@ -115,22 +115,22 @@ public class Main {
                     }
                 }
             }
-            System.out.println("Would you like to play again?");//ask if the user wants to keep going
+            System.out.println("다시할래?");//ask if the user wants to keep going
             Scanner yesorno = new Scanner(System.in);
             String answer = yesorno.nextLine();
             while (!isyesorno(answer)) {
-                System.out.println("Please answer yes or no.");
+                System.out.println("yes or no 입력해도!!");
                 answer = yesorno.nextLine();
             }
             if (answer.equals("no")) {
                 break;
             }
         }
-        System.out.println("Your cash is: " + cash);//if user doesn't want to play or runs out of cash, either congratulates them on their winnings or lets them know
+        System.out.println("너의 돈: " + cash);//if user doesn't want to play or runs out of cash, either congratulates them on their winnings or lets them know
         if (cash == 0) {
-            System.out.println("You ran out of cash!");
+            System.out.println("너 오링!");
         } else {
-            System.out.println("Enjoy your winnings, " + name + "!");
+            System.out.println("축하해 이겼어!, " + name + "!");
         }
     }
 
@@ -171,8 +171,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int bet = sc.nextInt();
         while (bet > cash) {
-            System.out.println("You cannot bet more cash than you have!");
-            System.out.println("How much would you like to bet?");
+            System.out.println("기본 배팅돈도 없네!");
+            System.out.println("얼마 걸래?");
             bet = sc.nextInt();
         }
         return bet;
@@ -182,27 +182,27 @@ public class Main {
      * Called if the user wins.
      */
     public static void Win() {
-        System.out.println("Congratulations, you win!");
+        System.out.println("니가 이겼다 축하해!");
         cash = cash + bet;
-        System.out.println("Cash: " + cash);
+        System.out.println("현금: " + cash);
     }
 
     /*
      * Called if the user loses.
      */
     public static void Lose() {
-        System.out.println("Sorry, you lose!");
+        System.out.println("니가 졌다!");
         cash = cash - bet;
-        System.out.println("Cash: " + cash);
+        System.out.println("현금: " + cash);
     }
 
     /*
      * Called if the user pushes
      */
     public static void Push() {
-        System.out.println("It's a push!");
-        System.out.println("You get your money back.");
-        System.out.println("Cash: " + cash);
+        System.out.println("푸시!");
+        System.out.println("돈 다시 갖고 가라.");
+        System.out.println("현금: " + cash);
     }
 
     /*
@@ -240,7 +240,7 @@ public class Main {
      */
     public static boolean checkBust(int handvalue) {
         if (handvalue > 21) {
-            System.out.println("You have busted!");
+            System.out.println("버스트!");
             return true;
         }
         return false;
@@ -260,7 +260,7 @@ public class Main {
      * Called if the user has a five card trick.
      */
     public static void fivecardtrick() {
-        System.out.println("You have achieved a five card trick!");
+        System.out.println("카드 다섯장이네! 니가 이겼어!");
         Win();
     }
 }
