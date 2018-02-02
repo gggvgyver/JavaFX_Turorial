1c2dbbfc1a4f699fd3b39c256c4417a3a22e514c
gyver <gggvgyver@gmail.com>
2018-02-02 10:27:41 +0900

    도서관에서 180201

--- src/GuiDemo/GuiController.java
+++ src/GuiDemo/GuiController.java
@@ -1,4 +1,49 @@
 package GuiDemo;
 
-public class GuiController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GuiController implements Initializable {
+
+    //체크박스
+    @FXML private Label viewLabelOne; //라벨: 결과값1
+    @FXML private CheckBox firstCheckBox; //체크박스1
+    @FXML private CheckBox secondCheckBox; //체크박스2
+    @FXML private CheckBox thirdCheckBox; //체크박스3
+
+    //선택박스
+    @FXML private Label viewLabelTwo; //라벨: 결과값2
+    @FXML private ChoiceBox firstChoiceBox; //선택박스1
+
+    public void checkButtonPushed() {
+        String order = "니가 체크한것은: ";
+
+        if(firstCheckBox.isSelected()) order += "\n파페로니";
+        if(secondCheckBox.isSelected()) order += "\n파인애플";
+        if(thirdCheckBox.isSelected()) order += "\n베이컨";
+
+        this.viewLabelOne.setText(order);
+    }
+
+    public void choiceButtonPushed() {
+        this.viewLabelTwo.setText(firstChoiceBox.getValue().toString());
+    }
+
+    public void initialize(URL url, ResourceBundle rb) {
+        //체크박스
+        viewLabelOne.setText("");
+
+        //선택박스
+        viewLabelTwo.setText("");
+        firstChoiceBox.getItems().add("사과");
+        firstChoiceBox.getItems().add("배");
+        firstChoiceBox.getItems().addAll("바나나", "오렌지", "감", "수박");
+
+    }
 }
--- src/GuiDemo/Main.java
+++ src/GuiDemo/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("Gui.fxml"));
-            Scene scene = new Scene(root, 600, 400);
+            Scene scene = new Scene(root, 800, 600);
             scene.getStylesheets().add(getClass().getResource("Gui.css").toExternalForm());
             primaryStage.setScene(scene);
             primaryStage.setTitle("도서관에서 자바FX GUI 연습 180202");
