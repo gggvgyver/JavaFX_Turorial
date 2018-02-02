5556b982715a8beb6356a33711ec184dc62b2eed
gyver <gggvgyver@gmail.com>
2018-02-02 13:12:09 +0900

    자바GUI연습 도서관에서 180202

--- src/GuiDemo/GuiController.java
+++ src/GuiDemo/GuiController.java
@@ -2,9 +2,7 @@ package GuiDemo;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,6 +18,22 @@ public class GuiController implements Initializable {
     //선택박스
     @FXML private Label choiceLabel; //라벨: 선택 결과값
     @FXML private ChoiceBox firstChoiceBox; //선택박스1
+
+    //콤보박스
+    @FXML private Label comboLabel; //라벨: 콤보 결과값
+    @FXML private ComboBox firstComboBox; //콤보박스1
+
+    //라디오박스
+    @FXML private Label radioLabel; //라벨: 라디오 결과값
+    @FXML private RadioButton firstRadioBox; //라디오박스1
+    @FXML private RadioButton secondRadioBox; //라디오박스2
+    @FXML private RadioButton thirdRadioBox; //라디오박스3
+    @FXML private RadioButton fourthRadioBox; //라디오박스4
+
+    //라디오박스 중복불가
+    private ToggleGroup radioToggleGroup;
+
+    //------------------------------------------------------
 
     //체크박스 결과
     public void checkButtonPushed() {
@@ -37,15 +51,53 @@ public class GuiController implements Initializable {
         this.choiceLabel.setText(firstChoiceBox.getValue().toString());
     }
 
+    //콤보박스 결과
+    public void comboButtonPushed() {
+        this.comboLabel.setText("니가 선택한 콤보는:\n" + firstComboBox.getValue().toString());
+    }
+
+    //라디오박스 결과 표현1
+    public  void radioButtonPushed() {
+        String order = "라디오값: ";
+
+        if(firstRadioBox.isSelected()) order += "\n자바";
+        if(secondRadioBox.isSelected()) order += "\n파이썬";
+        if(thirdRadioBox.isSelected()) order += "\n비주얼베이직";
+        if(fourthRadioBox.isSelected()) order += "\nPHP";
+
+        this.radioLabel.setText(order);
+    }
+
+    //라디오박스 결과 표현2
+    public  void radioButtonPushed2() {
+        if(this.radioToggleGroup.getSelectedToggle().equals(this.firstRadioBox)) radioLabel.setText("자바");
+        if(this.radioToggleGroup.getSelectedToggle().equals(this.secondRadioBox)) radioLabel.setText("파이썬");
+        if(this.radioToggleGroup.getSelectedToggle().equals(this.thirdRadioBox)) radioLabel.setText("비주얼베이직");
+        if(this.radioToggleGroup.getSelectedToggle().equals(this.fourthRadioBox)) radioLabel.setText("PHP");
+    }
+
+    //------------------------------------------------------
+
     public void initialize(URL url, ResourceBundle rb) {
         //체크박스 초기값
         checkLabel.setText("");
 
         //선택박스 초기값
-        choiceLabel.setText("");
+        choiceLabel.setText("선택하세요!");
         firstChoiceBox.getItems().add("사과");
         firstChoiceBox.getItems().add("배");
         firstChoiceBox.getItems().addAll("바나나", "오렌지", "감", "수박");
 
+        //콤보박스 초기값
+        firstComboBox.getItems().add("노트북");
+        firstComboBox.getItems().addAll("TV", "냉장고", "세탁기");
+
+        //라디오박스 초기값
+        radioLabel.setText("");
+        radioToggleGroup = new ToggleGroup();
+        this.firstRadioBox.setToggleGroup(radioToggleGroup);
+        this.secondRadioBox.setToggleGroup(radioToggleGroup);
+        this.thirdRadioBox.setToggleGroup(radioToggleGroup);
+        this.fourthRadioBox.setToggleGroup(radioToggleGroup);
     }
 }
