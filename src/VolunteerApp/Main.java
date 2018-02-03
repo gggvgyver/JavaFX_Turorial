922ccf13d2430465d0a165eca4cdb57812907ae1
gyver <gggvgyver@gmail.com>
2018-02-03 12:38:49 +0900

    집에서 Volunteer App 연습 180203

---
+++ src/VolunteerApp/Main.java
@@ -0,0 +1,4 @@
+package VolunteerApp;
+
+public class Main {
+}
--- src/VolunteerApp/models/Volunteer.java
+++ src/VolunteerApp/models/Volunteer.java
@@ -8,5 +8,43 @@ public class Volunteer {
     private LocalDate birthDay;
     private File imageFile;
 
+    public Volunteer(String firstName, String lastName, String phoneNumber, LocalDate birthDay) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.birthDay = birthDay;
+        this.imageFile = new File("./images/defaultPerson.jpg");
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(LocalDate birthDay) {
+        this.birthDay = birthDay;
+    }
 }
