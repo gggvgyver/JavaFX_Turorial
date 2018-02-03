883914b80115df17d79754d722021b0a7be332bc
gyver <gggvgyver@gmail.com>
2018-02-03 11:45:55 +0900

    집에서 Volunteer App 연습 180203

---
+++ src/VolunteerApp/models/Volunteer.java
@@ -0,0 +1,12 @@
+package VolunteerApp.models;
+
+import java.io.File;
+import java.time.LocalDate;
+
+public class Volunteer {
+    private String firstName, lastName, phoneNumber;
+    private LocalDate birthDay;
+    private File imageFile;
+
+
+}
