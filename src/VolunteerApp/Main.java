633af3b5c52b53c95d14fd712f9d306b8f01075d
gyver <gggvgyver@gmail.com>
2018-02-03 13:46:39 +0900

    집에서 Volunteer App 연습 180203

--- src/VolunteerApp/Main.java
+++ src/VolunteerApp/Main.java
@@ -1,4 +1,26 @@
 package VolunteerApp;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("./views/LoginView.fxml"));
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
